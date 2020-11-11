ca650dcb3924212f37be321621e66e67dce949ff
Christopher <chrisobrien416@gmail.com>
2020-11-10 19:24:35 -0500

    routing

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const NotesService = require('./notes/notes-service')
+const notesRouter = require('./notes/notes-router')
 const app = express()
 const morganOption = (NODE_ENV === 'production' ? 'tiny' : 'common')
 app.use(morgan(morganOption))
@@ -15,55 +15,17 @@ app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
 
-app.get('/notes', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    NotesService.getAllNotes(knexInstance)
-        .then(notes => {
-            res.json(notes)
-        })
-        .catch(next)
-})
+app.use('/notes', notesRouter)
 
-app.get('/notes/:note_id', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    NotesService.getById(knexInstance, req.params.note_id)
-        .then(note => {
-            if(!note) {
-                return res.status(404).json({
-                    error: { message: `Note doesn't exist`}
-                })
-            }
-            res.json(note)
-        })
-        .catch(next)
-})
-
-app.post('/notes', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    console.log(req.body)
-    const { note_name, content, folder_id } = req.body
-    const newNote = { note_name, content, folder_id}
-    NotesService.insertNote(knexInstance, newNote)
-        .then(note => {
-            res
-                .status(201)
-                .location(`/notes/${note.id}`)
-                .json(note)
-        })
-        .catch(next)
-})
-
-app.use(function errorHandler(e, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: {message: 'server error'}}
-    } else {
-        console.error(error)
-        response = {message: error.message, error}
-    }
-    res.status(500).json(response)
-})
-
-
+// app.use(function errorHandler(e, req, res, next) {
+//     let response
+//     if (NODE_ENV === 'production') {
+//         response = { error: {message: 'server error'}}
+//     } else {
+//         console.error(error)
+//         response = {message: error.message, error}
+//     }
+//     res.status(500).json(response)
+// })
 
 module.exports = app
---
+++ src/notes/notes-router.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const NotesService = require('./notes-service')
+const notesRouter = express.Router()
+const jsonParser = express.json()
+
+notesRouter
+    .route('/')
+    .get((req, res, next) => {
+    const knexInstance = req.app.get('db')
+    NotesService.getAllNotes(knexInstance)
+        .then(notes => {
+            res.json(notes)
+        })
+        .catch(next)
+})
+    .post(jsonParser, (req, res, next) => {
+    console.log(req.body)
+    const knexInstance = req.app.get('db')
+    const { note_name, content, folder_id } = req.body
+    const newNote = { note_name, content, folder_id}
+    for (const [key, value] of Object.entries(newNote)) {
+        if (value == null) {
+            return res.status(400).json({
+                error: { message: `Missing ${key} in request body`}
+            })
+        }
+    }
+    NotesService.insertNote(knexInstance, newNote)
+        .then(note => {
+            res
+                .status(201)
+                .location(`/notes/${note.id}`)
+                .json(note)
+        })
+        .catch(next)
+})
+
+notesRouter
+    .route('/:note_id')
+    .get((req, res, next) => {
+    const knexInstance = req.app.get('db')
+    NotesService.getById(knexInstance, req.params.note_id)
+        .then(note => {
+            if(!note) {
+                return res.status(404).json({
+                    error: { message: `Note doesn't exist`}
+                })
+            }
+            res.json(note)
+        })
+        .catch(next)
+})
+
+module.exports = notesRouter
